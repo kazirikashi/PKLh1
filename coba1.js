de60e3822aa6676f6c75b454fc6e7cb189ce1046
kazirikashi <kkcilacap03@gmail.com>
2023-07-03 18:09:30 +0700

    Add files via upload

---
+++ coba1.js
@@ -0,0 +1,14 @@
+function sayHello(person) {
+  console.log(person.firstName);
+  console.log(person.lastName);
+  console.log(person.age);
+  console.log(person.eyeColor);
+}
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+sayHello(person);
